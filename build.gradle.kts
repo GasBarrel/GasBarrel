54cf218137ae7a6566ea8ecbcb472aacc4fc49a1
Marcel Korzonek <bquickzz0@gmail.com>
2023-05-18 15:07:57 +0200

    Add dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.8.20"
-    id("com.github.johnrengelman.shadow") version "7.1.1"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
     application
 }
 
@@ -11,9 +11,25 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven("https://jitpack.io")
 }
 
 dependencies {
+    // JDA
+    implementation("net.dv8tion:JDA:5.0.0-beta.9")
+    implementation("io.github.minndevelopment:jda-ktx:9fc90f616b")
+
+    // Application commands
+    implementation("io.github.freya022:BotCommands:583b02c4f0")
+
+    // Logging
+    implementation("ch.qos.logback:logback-classic:1.4.5")
+    implementation("io.github.microutils:kotlin-logging-jvm:3.0.4")
+
+    // Kotlin
+    implementation("org.jetbrains.kotlin:kotlin-reflect:1.8.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+
     testImplementation(kotlin("test"))
 }
 
