2968382e688b8a7cbdd326780d3d8a8e2135d29d
RGBCube <RGBCube@users.noreply.github.com>
2023-05-19 16:14:40 +0300

    Seperate Data and Config well, Make dev and prod configurations consistent

--- src/main/kotlin/io/github/gasbarrel/Config.kt
+++ src/main/kotlin/io/github/gasbarrel/Config.kt
@@ -3,20 +3,33 @@ package io.github.gasbarrel
 import com.akuleshov7.ktoml.Toml
 import com.freya02.botcommands.api.core.annotations.BService
 import com.freya02.botcommands.api.core.suppliers.annotations.InstanceSupplier
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.decodeFromString
 import mu.KotlinLogging
+import kotlin.io.path.Path
+import kotlin.io.path.exists
 import kotlin.io.path.readText
+
+val config: Config by lazy {
+    val configPath = Config.folder.resolve("config.toml")
+
+    Config.logger.info("Loading configuration at $configPath")
+
+    return@lazy Toml.decodeFromString(configPath.readText())
+}
 
 @Serializable
 data class DatabaseConfig(
+    @SerialName("server-name")
     val serverName: String,
     val port: Int,
     val name: String,
     val user: String,
     val password: String
 ) {
-    val url: String = "jdbc:postgresql://$serverName:$port/$name"
+    val url: String
+        get() = "jdbc:postgresql://$serverName:$port/$name"
 }
 
 @BService
@@ -24,22 +37,22 @@ data class DatabaseConfig(
 data class Config(
     val token: String,
     val prefixes: List<String>,
+    @SerialName("owner-ids")
     val ownerIds: List<Long>,
+    @SerialName("test-guild-ids")
     val testGuildIds: List<Long>,
     val database: DatabaseConfig
 ) {
     companion object {
-        private val logger = KotlinLogging.logger {}
+        internal val logger = KotlinLogging.logger {}
 
-        val config: Config by lazy {
-            val configPath = Data.getEffectiveConfigPath()
+        /**
+         * The mode is determined by checking whether if the
+         * `dev-config` directory exists in the current directory.
+         */
+        val isDevEnvironment = Path("dev-config").exists()
 
-            if (Data.isDevEnvironment) {
-                logger.info("Loading test config")
-            }
-
-            return@lazy Toml.decodeFromString(configPath.readText())
-        }
+        val folder = Path(".", if (isDevEnvironment) "dev-config" else "config")
 
         @InstanceSupplier
         fun supply(): Config = config
--- src/main/kotlin/io/github/gasbarrel/Data.kt
+++ src/main/kotlin/io/github/gasbarrel/Data.kt
@@ -1,65 +1,7 @@
 package io.github.gasbarrel
 
-import java.io.FileNotFoundException
-import java.nio.file.Path
-import kotlin.io.path.*
+import kotlin.io.path.Path
 
-// If dev configs exists then they should be loaded
-// If not then fallback to the config path, which must be validated
-// Other configs such as logback are set in the bot folder
 object Data {
-    private val devConfigPath: Path = Path("config.dev.toml")
-
-    val isDevEnvironment = devConfigPath.exists()
-
-    /**
-     * * Dev: Project's root
-     * * Prod: Folder in which the jar is in
-     */
-    private val botFolder: Path = getBotFolder()
-
-    private val configFolder: Path = botFolder.resolve("config")
-    private val dataFolder: Path = getDataFolder()
-
-    //Fine if not used, might just be using the dev config
-    private val prodConfigPath: Path = configFolder.resolve("config.toml")
-    val prodLogbackConfigPath: Path = configFolder.resolve("logback.xml")
-
-    fun init() {
-        if (!isDevEnvironment) {
-            prodConfigPath.validate("Bot config")
-            prodLogbackConfigPath.validate("Logback config")
-        }
-
-        //Create optional folders
-        dataFolder.createDirectories()
-    }
-
-    fun getEffectiveConfigPath(): Path = when {
-        isDevEnvironment -> devConfigPath
-        prodConfigPath.exists() -> prodConfigPath
-        else -> throw FileNotFoundException("Bot config at '${prodConfigPath.absolutePathString()}' does not exist and dev config at '${devConfigPath.absolutePathString()}' was not found either.")
-    }
-
-    private fun Path.validate(desc: String) = this.also {
-        if (this.notExists())
-            throw FileNotFoundException("$desc at ${this.absolutePathString()} does not exist.")
-    }
-
-    private fun getBotFolder() = when {
-        isDevEnvironment -> Path("")
-        else -> {
-            val jarPath = javaClass.protectionDomain.codeSource.location.toURI().toPath()
-            if (jarPath.extension != "jar") {
-                throw IllegalStateException("Production environment detected (no '${devConfigPath.name}'), but file at $jarPath isn't a JAR")
-            }
-
-            jarPath.parent
-        }
-    }
-
-    private fun getDataFolder() = when {
-        isDevEnvironment -> botFolder.resolve("dev-data")
-        else -> botFolder.resolve("data")
-    }
+    val folder = Path(".", if (Config.isDevEnvironment) "dev-data" else "data")
 }
