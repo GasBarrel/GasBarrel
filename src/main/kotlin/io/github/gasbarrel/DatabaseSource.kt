eeb2979e07eaf47364434ea928961bef27a6e481
RGBCube <RGBCube@users.noreply.github.com>
2023-05-19 16:15:36 +0300

    Clean DatabaseSource.kt

--- src/main/kotlin/io/github/gasbarrel/DatabaseSource.kt
+++ src/main/kotlin/io/github/gasbarrel/DatabaseSource.kt
@@ -12,28 +12,27 @@ import java.sql.Connection
 import kotlin.io.path.*
 import kotlin.time.Duration.Companion.seconds
 
+private val migrationNameRegex = Regex("""v(\d+)\.(\d+)__.+\.sql""")
+private val databaseVersionRegex = Regex("""(\d+)\.(\d+)""")
+
 @BService(ServiceStart.PRE_LOAD)
 @ServiceType(type = ConnectionSupplier::class)
 class DatabaseSource(config: Config) : ConnectionSupplier {
-    private val version = "1.0" //Same version as in CreateDatabase.sql
+    private val version = "1.0" // Same version as in CreateDatabase.sql
+
     private val source: HikariDataSource
 
-    private val migrationNameRegex = Regex("""v(\d+)\.(\d+)__.+\.sql""")
-    private val dbVersionRegex = Regex("""(\d+)\.(\d+)""")
-
     init {
-        val dbConfig = config.dbConfig
+        val databaseConfig = config.database
 
-        val hikariConfig = HikariConfig().apply {
-            jdbcUrl = dbConfig.dbURL
-            username = dbConfig.user
-            password = dbConfig.password
+        source = HikariDataSource(HikariConfig().apply {
+            jdbcUrl = databaseConfig.url
+            username = databaseConfig.user
+            password = databaseConfig.password
 
             maximumPoolSize = 2
             leakDetectionThreshold = 10.seconds.inWholeMilliseconds
-        }
-
-        source = HikariDataSource(hikariConfig)
+        })
 
         checkVersion()
 
@@ -42,19 +41,21 @@ class DatabaseSource(config: Config) : ConnectionSupplier {
 
     private fun checkVersion() {
         source.connection.use { connection ->
-            connection.prepareStatement("select version from gasbarrel_version").use { statement ->
+            connection.prepareStatement("SELECT version FROM gasbarrel_version").use { statement ->
                 statement.executeQuery().use { rs ->
                     if (!rs.next()) throw IllegalStateException("Found no version in database, please refer to the README to set up the database")
 
-                    val dbVersion = rs.getString("version")
-                    if (dbVersion != version) {
-                        val sqlFolderPath = Path("sql")
-                        val suffix = when {
-                            sqlFolderPath.exists() -> buildHintSuffix(sqlFolderPath, dbVersion)
-                            else -> ""
-                        }
+                    val databaseVersion = rs.getString("version")
 
-                        throw IllegalStateException("Database version mismatch, expected $version, database version is $dbVersion $suffix")
+                    if (databaseVersion != version) {
+                        val sqlFolderPath = Path("sql")
+
+                        val suffix = if (sqlFolderPath.exists())
+                            buildHintSuffix(sqlFolderPath, databaseVersion)
+                        else
+                            ""
+
+                        throw IllegalStateException("Database version mismatch, expected $version, got $databaseVersion $suffix")
                     }
                 }
             }
@@ -62,14 +63,14 @@ class DatabaseSource(config: Config) : ConnectionSupplier {
     }
 
     @OptIn(ExperimentalPathApi::class)
-    private fun buildHintSuffix(sqlFolderPath: Path, dbVersion: String): String {
+    private fun buildHintSuffix(sqlFolderPath: Path, databaseVersion: String): String {
         val hintFiles = sqlFolderPath.walk()
             .filter { it.extension == "sql" }
             .filter {
                 val (_, major, minor) = migrationNameRegex.matchEntire(it.name)?.groupValues ?: return@filter false
-                val (_, dbMajor, dbMinor) = dbVersionRegex.matchEntire(dbVersion)?.groupValues ?: return@filter false
+                val (_, dbMajor, dbMinor) = databaseVersionRegex.matchEntire(databaseVersion)?.groupValues ?: return@filter false
 
-                //Keep if db version is lower than file
+                // Keep if db version is lower than file
                 if (dbMajor.toInt() < major.toInt()) return@filter true
                 if (dbMinor.toInt() < minor.toInt()) return@filter true
 
